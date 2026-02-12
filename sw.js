387eb1a393ecbfe09186d60cbb8b455bd16a99e4
rudrabhavsar008 <rudrabhavsar008@gmail.com>
2026-02-12 23:58:05 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// Version: final-v1 (Changing this name forces an update)
+const cacheName = 'timetable-final-v1'; 
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install Event: Caches the files
+self.addEventListener('install', e => {
+  self.skipWaiting(); // Forces this new worker to become active immediately
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activate Event: Cleans up old versions
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch Event: Serves files from Cache if offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
